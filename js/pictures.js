bd3eaed8ba1e9787d76ad671699f073b2a76a412
Sergey Zveroboev <szveroboev@gmail.com>
2017-08-18 04:44:39 +0300

    Заполнил index.html фотографиями. Написал функцию открытия просмотра фото

--- js/pictures.js
+++ js/pictures.js
@@ -1,8 +1,12 @@
 'use strict';
 
+// Нужно ли эти константы объеденить в один объект?
 var MIN_LIKES = 15;
 var MAX_LIKES = 200;
-var NUMBER_PHOTOS = 25;
+
+// В ДЗ указано 25 фотографий, но их в папке 26 и на экран помещается 26. Если оставить 25 как указано в ДЗ,
+// то в нижнем правом углу экрана будет дырка, будто этой 26ой и не хватает. Если что - верну обратно на 25.
+var PHOTO_QUANTITY = 26;
 var PICTURES_COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -20,27 +24,77 @@ function getRandomIndex(number) {
   return Math.floor(Math.random() * number);
 }
 
+function getRandomComment(array) {
+  var firstRandomComment = array[getRandomIndex(array.length)];
+  var secondRandomComment = array[getRandomIndex(array.length)];
+
+  // Стоить ли создать пустой массив с комментариями и пушить в него случайный комментарий через цикл,
+  // где i = 1 / i = 2 будет выбираться через функцию 'getRandomValue'?
+  return getRandomValue(1, 2) === 1 ? [firstRandomComment] : [firstRandomComment, secondRandomComment];
+  // Надо еще как-то реализовать, что бы 2 одинаковых значения подряд не выдовало,
+  // но я ночью туплю и не пойму как. Позже может додумаюсь.
+}
+
 function getPostsArray(number) {
   var postsArray = [];
 
   for (var i = 0; i < number; i++) {
     postsArray.push({
-      url: 'photos/{{' + (i + 1) + '}}.jpg',
+      url: 'photos/' + (i + 1) + '.jpg',
       likes: getRandomValue(MIN_LIKES, MAX_LIKES),
-      comment: PICTURES_COMMENTS[getRandomIndex(PICTURES_COMMENTS.length)]
+      comment: getRandomComment(PICTURES_COMMENTS)
     });
   }
   return postsArray;
 }
 
-function renderPost(posts) {
-  var postElement = pictureTemplate.cloneNode(true);
+function renderPostStructure(posts) {
+  // Может эту переменную 'postStructure' стоить вынести за пределы этой функции,
+  // что бы не копировать содержимое шаблона в каждой итерации цикла?
+  // Например вынести в глобал (что не есть хорошо, вроде) или в функцию 'renderPosts',
+  // которая создает картинки на главной из этой структуры. Тогда переменная будет видна через замыкание.
+  var postStructure = document.querySelector('#picture-template').content.cloneNode(true);
 
-  postElement.querySelector('img').setAttribute('src', posts.url);
-  postElement.querySelector('.picture-likes').textContent = posts.likes;
-  postElement.querySelector('.picture-comments').textContent = posts.comment;
-  return postElement;
+  postStructure.querySelector('img').setAttribute('src', posts.url);
+  postStructure.querySelector('.picture-likes').textContent = posts.likes;
+  postStructure.querySelector('.picture-comments').textContent = posts.comment.length;
+
+  return postStructure;
 }
 
-var posts = getPostsArray(NUMBER_PHOTOS);
-var pictureTemplate = document.querySelector('#picture-template').content;
+function hideUploadForm() {
+  document.querySelector('.upload-form').classList.add('hidden');
+}
+
+function showGallery(arrayElement) {
+  var galleryElement = document.querySelector('.gallery-overlay');
+
+  hideUploadForm();
+  galleryElement.classList.remove('hidden');
+
+  // Тут, похоже, DRY не соблюдается. В функции 'renderPostStructure' мы производим похожие действия
+  // но делает она их совершенно для другого. Не знаю есть ли смысл вызывать ее из этой функции.
+  // Темболее у них разные селекторы в поиске. Это придется дополнительно 4 параметра в нее передавать.
+  // Если 'renderPostStructure' вызывать еще и отсюда, то переменную 'postStructure' из нее не получиться перенести
+  // в функцию 'renderPost'.
+  galleryElement.querySelector('.gallery-overlay-image').setAttribute('src', arrayElement.url);
+  galleryElement.querySelector('.likes-count').textContent = arrayElement.likes;
+  galleryElement.querySelector('.comments-count').textContent = arrayElement.comment.length;
+}
+
+function renderPosts() {
+  var posts = getPostsArray(PHOTO_QUANTITY);
+  var picturesElement = document.querySelector('.pictures');
+
+  for (var i = 0; i < PHOTO_QUANTITY; i++) {
+    picturesElement.appendChild(renderPostStructure(posts[i]));
+  }
+  return posts;
+}
+
+renderPosts();
+
+// Запускаю функцию 'showGallery' отдельно что бы удобнее было ее исключать из кода.
+// Все-равно она потом на обработчик события повесится... наеврное.
+showGallery(renderPosts()[0]);
+
