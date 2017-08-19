e1465206a38a8d10b5dd42bcea0c39ae0fb315d6
Sergey Zveroboev <szveroboev@gmail.com>
2017-08-19 18:10:41 +0300

    Переделал функцию получения массива со случайными комментариями. Учел повтор одинаковых комментариев внутри одного поста

--- js/pictures.js
+++ js/pictures.js
@@ -1,11 +1,7 @@
 'use strict';
 
-// Нужно ли  все эти 4 константы объеденить в один объект?
 var MIN_LIKES = 15;
 var MAX_LIKES = 200;
-
-// В ДЗ указано 25 фотографий, но их в папке 26 и на экран помещается 26. Если оставить 25 как указано в ДЗ,
-// то в нижнем правом углу экрана будет дырка, будто этой 26ой и не хватает. Если что - верну обратно на 25.
 var PHOTO_QUANTITY = 26;
 var PICTURES_COMMENTS = [
   'Всё отлично!',
@@ -24,15 +20,28 @@ function getRandomIndex(number) {
   return Math.floor(Math.random() * number);
 }
 
-function getRandomComment(array) {
-  var firstRandomComment = array[getRandomIndex(array.length)];
-  var secondRandomComment = array[getRandomIndex(array.length)];
+function getRandomComments(number) {
+  var comments = [];
+  var repeatedIndex = -1;
 
-  // Стоить ли создать пустой массив с комментариями и пушить в него случайный комментарий через цикл,
-  // где i = 1 / i = 2 будет выбираться через функцию 'getRandomValue'?
-  return getRandomValue(1, 2) === 1 ? [firstRandomComment] : [firstRandomComment, secondRandomComment];
-  // Надо еще как-то реализовать, что бы 2 одинаковых значения подряд не выдовало,
-  // но я ночью туплю и не пойму как. Позже может додумаюсь.
+  for (var i = 0; i < number; i++) {
+    var randomIndex = getRandomIndex(PICTURES_COMMENTS.length);
+
+    if (randomIndex === repeatedIndex) {
+      for (var j = 0; ; j++) {
+        randomIndex = getRandomIndex(PICTURES_COMMENTS.length);
+        if (randomIndex !== repeatedIndex) {
+          break;
+        }
+      }
+      comments.push(PICTURES_COMMENTS[randomIndex]);
+      repeatedIndex = randomIndex;
+    } else {
+      comments.push(PICTURES_COMMENTS[randomIndex]);
+      repeatedIndex = randomIndex;
+    }
+  }
+  return comments;
 }
 
 function getPostsArray(number) {
@@ -42,17 +51,13 @@ function getPostsArray(number) {
     postsArray.push({
       url: 'photos/' + (i + 1) + '.jpg',
       likes: getRandomValue(MIN_LIKES, MAX_LIKES),
-      comment: getRandomComment(PICTURES_COMMENTS)
+      comment: getRandomComments(getRandomValue(1, 2))
     });
   }
   return postsArray;
 }
 
 function renderPostStructure(posts) {
-  // Может эту переменную 'postStructure' стоить вынести за пределы этой функции,
-  // что бы не копировать содержимое шаблона в каждой итерации цикла?
-  // Например вынести в глобал (что не есть хорошо, вроде) или в функцию 'renderPosts',
-  // которая создает картинки на главной из этой структуры. Тогда переменная будет видна через замыкание.
   var postStructure = document.querySelector('#picture-template').content.cloneNode(true);
 
   postStructure.querySelector('img').setAttribute('src', posts.url);
@@ -71,12 +76,6 @@ function showGallery(arrayElement) {
 
   hideUploadForm();
   galleryElement.classList.remove('hidden');
-
-  // Тут, похоже, DRY не соблюдается. В функции 'renderPostStructure' мы производим похожие действия
-  // но делает она их совершенно для другого. Не знаю есть ли смысл вызывать ее из этой функции.
-  // Темболее у них разные селекторы в поиске. Это придется дополнительно 4 параметра в нее передавать.
-  // Если 'renderPostStructure' вызывать еще и отсюда, то переменную 'postStructure' из нее не получиться перенести
-  // в функцию 'renderPost'.
   galleryElement.querySelector('.gallery-overlay-image').setAttribute('src', arrayElement.url);
   galleryElement.querySelector('.likes-count').textContent = arrayElement.likes;
   galleryElement.querySelector('.comments-count').textContent = arrayElement.comment.length;
@@ -92,12 +91,8 @@ function renderPosts() {
   }
   picturesElement.appendChild(fragment);
 
-  return posts;
+  showGallery(posts[0]);
 }
 
 renderPosts();
-
-// Запускаю функцию 'showGallery' отдельно что бы удобнее было ее исключать из кода.
-// Все-равно она потом на обработчик события повесится... наеврное.
-showGallery(renderPosts()[0]);
 
